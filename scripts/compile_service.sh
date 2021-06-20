5d7df65fa7a907d3a177d82e222d7ec68994dedd
Tianhao <tianhaoz@umich.edu>
2021-06-20 07:12:19 +0000

    wip: not working...

--- scripts/compile_service.sh
+++ scripts/compile_service.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
+PROJECT_ROOT="/workspaces/dart-grpc-rest-2-in-1"
+PROTO_ROOT="$PROJECT_ROOT/proto"
+RPC_GEN_ROOT="$PROJECT_ROOT/grpc-server/lib/generated/grpc"
+REST_GEN_ROOT="$PROJECT_ROOT/grpc-server/lib/generated/rest"
 
+protoc -I "$PROTO_ROOT" \
+    --dart_out="grpc:$RPC_GEN_ROOT" \
+    --grpc-gateway_out="$REST_GEN_ROOT" \
+    --grpc-gateway_opt logtostderr=true \
+    --grpc-gateway_opt paths=source_relative \
+    --grpc-gateway_opt generate_unbound_methods=true \
+    "$PROTO_ROOT/hello.proto"
--- scripts/init_devcontainer.sh
+++ scripts/init_devcontainer.sh
@@ -3,3 +3,9 @@
 dart pub global activate protoc_plugin
 
 cd ./grpc-server && pub get && cd ..
+
+go get \
+    github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway \
+    github.com/grpc-ecosystem/grpc-gateway/protoc-gen-openapiv2 \
+    google.golang.org/protobuf/cmd/protoc-gen-go \
+    google.golang.org/grpc/cmd/protoc-gen-go-grpc
