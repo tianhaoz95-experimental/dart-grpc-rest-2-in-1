2060e29a5a9cbf88a37d06a345a50fd7955b8e6a
Tianhao <tianhaoz@umich.edu>
2021-06-20 07:33:00 +0000

    fix: remove some opt

--- scripts/compile_service.sh
+++ scripts/compile_service.sh
@@ -8,7 +8,4 @@ REST_GEN_ROOT="$PROJECT_ROOT/grpc-server/lib/generated/rest"
 protoc -I "$PROTO_ROOT" \
     --dart_out="grpc:$RPC_GEN_ROOT" \
     --grpc-gateway_out="$REST_GEN_ROOT" \
-    --grpc-gateway_opt logtostderr=true \
-    --grpc-gateway_opt paths=source_relative \
-    --grpc-gateway_opt generate_unbound_methods=true \
     "$PROTO_ROOT/hello.proto"
